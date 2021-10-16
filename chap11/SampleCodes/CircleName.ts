17711870fce973a4f8f6cc6e7ef93563c4db40cb
miruoon <rillakerika@gmail.com>
2021-10-16 21:33:38 +0900

    feat:chap11  wip:Add chap11-3 samplecodes

---
+++ chap11/SampleCodes/CircleName.ts
@@ -0,0 +1,21 @@
+class CircleName {
+  private value: string;
+  constructor(value: string) {
+    if (value === "") {
+      throw Error("ArgumentNullException value");
+    }
+    if (value.length < 3) {
+      throw Error("サークル名は３文字以上です");
+    }
+    if (value.length > 20) {
+      throw Error("サークル名は２０文字以下です");
+    }
+    this.value = value;
+  }
+  public getValue() {
+    return this.value;
+  }
+  public isEqual(other: CircleName) {
+    return this.value === other.value;
+  }
+}
